8131371f638604a4c53966b3b03c5aef682831e5
Andrzej Michnia <amichnia@gmail.com>
2017-10-24 14:55:41 +0200

    feat: fixes for both equatable and sequence parameters, template

--- Mocky/Classes/Parameter.swift
+++ Mocky/Classes/Parameter.swift
@@ -26,7 +26,7 @@ public class Matcher {
 
         let comparator = matchers.reversed().first { (current, _) -> Bool in
             return current.subjectType == mirror.subjectType
-        }?.1
+            }?.1
 
         return comparator as? (T,T) -> Bool
     }
@@ -35,83 +35,114 @@ public class Matcher {
 public enum Parameter<ValueType> {
     case any(ValueType.Type)
     case value(ValueType)
-    
+
     public static func ==(lhs: Parameter<ValueType>, rhs: Parameter<ValueType>) -> Bool {
-        print("Parameter not equatable")
-        switch (lhs, rhs) {
-            default: return true
-        }
+        debugPrint("Parameter not equatable \(ValueType.self)")
+        return true
     }
 
     public static func compare(lhs: Parameter<ValueType>, rhs: Parameter<ValueType>, with matcher: Matcher) -> Bool {
-        print("Parameter not equatable")
+        debugPrint("Parameter not equatable \(ValueType.self)")
         switch (lhs, rhs) {
-            case (.any, _): return true
-            case (_, .any): return true
-            case (.value(let lhsValue), .value(let rhsValue)):
-                guard let compare = matcher.comparator(for: ValueType.self) else {
-                    fatalError("No registered comparators for \(String(describing: ValueType.self))")
-                }
-                return compare(lhsValue,rhsValue)
-            default: return true
-        }
-    }
-}
-
-public extension Parameter where ValueType : Sequence {
-    
-    public static func ==<ValueType: Equatable>(lhs: Parameter<ValueType>, rhs: Parameter<ValueType>) -> Bool {
-        print("Parameter is sequence")
-        switch (lhs, rhs) {
-            case (.any, _): return true
-            case (_, .any): return true
-            case (.value(let lhsSequence), .value(let rhsSequence)):
-                return lhsSequence == rhsSequence
-            default: return false
-        }
-    }
-
-    public static func compare(lhs: Parameter<ValueType>, rhs: Parameter<ValueType>, with matcher: Matcher) -> Bool {
-        print("Parameter is sequence")
-        switch (lhs, rhs) {
-            case (.any, _): return true
-            case (_, .any): return true
-            case (.value(let lhsSequence), .value(let rhsSequence)):
-                let leftArray = lhsSequence.map { $0 }
-                let rightArray = rhsSequence.map { $0 }
-
-                guard leftArray.count == rightArray.count else { return false }
-
-                let values = (0..<leftArray.count).map { i -> (ValueType.Iterator.Element, ValueType.Iterator.Element) in
-                    return (leftArray[i],rightArray[i])
-                }
-
-                guard let comparator = matcher.comparator(for: ValueType.Iterator.Element.self) else {
-                    fatalError("No registered comparators for \(String(describing: ValueType.self))")
-                }
-
-                for (left,right) in values {
-                    guard comparator(left, right) else {
-                        return false
-                    }
-                }
-
-                return true
-            default: return false
+        case (.any, _): return true
+        case (_, .any): return true
+        case (.value(let lhsValue), .value(let rhsValue)):
+            guard let compare = matcher.comparator(for: ValueType.self) else {
+                fatalError("No registered comparators for \(String(describing: ValueType.self))")
+            }
+            return compare(lhsValue,rhsValue)
+        default: return true
         }
     }
 }
 
 public extension Parameter where ValueType: Equatable {
-    
     public static func ==(lhs: Parameter<ValueType>, rhs: Parameter<ValueType>) -> Bool {
-        print("Parameter is equatable")
+        debugPrint("Parameter is equatable \(ValueType.self)")
         switch (lhs, rhs) {
-            case (.any, _): return true
-            case (_, .any): return true
-            case (.value(let value1), .value(let value2)):
-                return value1 == value2
-            default: return false
+        case (.any, _): return true
+        case (_, .any): return true
+        case (.value(let value1), .value(let value2)):
+            return value1 == value2
+        default: return false
+        }
+    }
+
+    public static func compare(lhs: Parameter<ValueType>, rhs: Parameter<ValueType>, with matcher: Matcher) -> Bool {
+        debugPrint("Parameter is equatable \(ValueType.self)")
+        return lhs == rhs
+    }
+}
+
+public extension Parameter where ValueType: Sequence {
+    public static func compare(lhs: Parameter<ValueType>, rhs: Parameter<ValueType>, with matcher: Matcher) -> Bool {
+        debugPrint("Parameter is sequence \(ValueType.self) where \(ValueType.Element.self) is not Equatable")
+        switch (lhs, rhs) {
+        case (.any, _): return true
+        case (_, .any): return true
+        case (.value(let lhsSequence), .value(let rhsSequence)):
+            let leftArray = lhsSequence.map { $0 }
+            let rightArray = rhsSequence.map { $0 }
+
+            guard leftArray.count == rightArray.count else { return false }
+
+            let values = (0..<leftArray.count)
+                .map { i -> (ValueType.Element, ValueType.Element) in
+                    return ((leftArray[i]),(rightArray[i]))
+            }
+
+            guard let comparator = matcher.comparator(for: ValueType.Element.self) else {
+                fatalError("Not registered comparator for \(ValueType.Element.self)")
+            }
+
+            for (left,right) in values {
+                guard comparator(left, right) else {
+                    return false
+                }
+            }
+
+            return true
+        default: return false
+        }
+    }
+}
+
+public extension Parameter where ValueType: Sequence, ValueType.Element: Equatable {
+    public static func compare(lhs: Parameter<ValueType>, rhs: Parameter<ValueType>, with matcher: Matcher) -> Bool {
+        debugPrint("Parameter is sequence \(ValueType.self) where \(ValueType.Element.self) is Equatable")
+        switch (lhs, rhs) {
+        case (.any, _): return true
+        case (_, .any): return true
+        case (.value(let lhsSequence), .value(let rhsSequence)):
+            let leftArray = lhsSequence.map { $0 }
+            let rightArray = rhsSequence.map { $0 }
+
+            guard leftArray.count == rightArray.count else { return false }
+
+            let values = (0..<leftArray.count)
+                .map { i -> (ValueType.Element, ValueType.Element) in
+                    return ((leftArray[i]),(rightArray[i]))
+            }
+
+            for (left,right) in values {
+                guard left == right else { return false }
+            }
+
+            return true
+        default: return false
+        }
+    }
+}
+
+public extension Parameter where ValueType: Sequence, ValueType: Equatable, ValueType.Element: Equatable {
+    public static func ==(lhs: Parameter<ValueType>, rhs: Parameter<ValueType>) -> Bool {
+        debugPrint("Parameter is equatable sequence")
+        switch (lhs, rhs) {
+        case (.any, _): return true
+        case (_, .any): return true
+        case (.value(let lhsSequence), .value(let rhsSequence)):
+            return lhsSequence == rhsSequence
+        default: return false
         }
     }
 
