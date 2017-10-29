6fb4c7919211a861eae154937d93a2cd182c8836
Przemysław Wośko <przemyslaw.wosko@intive.com>
2017-10-29 21:33:23 +0100

    add support for Objective C protocols

---
+++ Example/Mocky/NonSwiftProtocol.swift
@@ -0,0 +1,16 @@
+//
+//  NonSwiftProtocol.swift
+//  Mocky_Example
+//
+//  Created by przemyslaw.wosko on 29/10/2017.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+import Foundation
+
+//sourcery: AutoMockable
+//sourcery: ObjcProtocol
+@objc protocol NonSwiftProtocol {
+    @objc optional func returnNoting()
+    func someMethod()
+}
