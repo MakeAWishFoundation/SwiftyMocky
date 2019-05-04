a70f76ffee41134dd3cf1f26832411a0864be9c6
Andrzej Michnia <amichnia@gmail.com>
2019-05-04 14:19:12 +0200

    Updated dependencies, renamed and extracted main elements

--- Package.swift
+++ Package.swift
@@ -6,19 +6,32 @@ import PackageDescription
 let package = Package(
     name: "mocky",
     products: [
-        .executable(name: "mocky", targets: ["SwiftyMocky-CLI"]),
+        .executable(name: "mocky", targets: ["SwiftyMockyCLI"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0")
+        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
+        .package(url: "https://github.com/tuist/xcodeproj.git", .upToNextMajor(from: "6.5.0")),
+        .package(url: "https://github.com/jianstm/Crayon", from: "0.0.1"),
+        .package(url: "https://github.com/kylef/Commander", from: "0.8.0"),
+        .package(url: "https://github.com/kylef/PathKit", from: "0.0.1")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
-            name: "SwiftyMocky-CLI",
-            dependencies: ["ShellOut"]),
+            name: "SwiftyMockyCLI",
+            dependencies: ["SwiftyMockyCLICore"]),
+        .target(
+            name: "SwiftyMockyCLICore",
+            dependencies: [
+                "ShellOut",
+                "Crayon",
+                "xcodeproj",
+                "Commander",
+                "PathKit"
+            ]),
         .testTarget(
-            name: "SwiftyMocky-CLITests",
-            dependencies: ["SwiftyMocky-CLI"]),
+            name: "SwiftyMockyCLICoreTests",
+            dependencies: ["SwiftyMockyCLICore"]),
     ]
 )
---
+++ Sources/SwiftyMockyCLICore/GenerateCommand.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+class GenerateCommand {
+    init(config: String) {
+
+    }
+}
