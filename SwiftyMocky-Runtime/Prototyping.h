0f6e0817683ff997eda87c9e153cffa0abd1b9e3
Andrzej Michnia <amichnia@gmail.com>
2019-05-25 10:35:50 +0200

    Extracted prototyping framework target for Carthage

---
+++ SwiftyMocky-Runtime/Prototyping.h
@@ -0,0 +1,19 @@
+//
+//  Prototyping.h
+//  Prototyping
+//
+//  Created by Andrzej Michnia on 25/05/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Prototyping.
+FOUNDATION_EXPORT double PrototypingVersionNumber;
+
+//! Project version string for Prototyping.
+FOUNDATION_EXPORT const unsigned char PrototypingVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Prototyping/PublicHeader.h>
+
+
