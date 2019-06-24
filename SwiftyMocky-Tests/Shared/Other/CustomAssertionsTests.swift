084b734e34ff136e1880f34a2959e0cfbcb72221
Andrzej Michnia <amichnia@gmail.com>
2019-06-24 11:46:59 +0200

    Fixed custom assertions, added tests.

---
+++ SwiftyMocky-Tests/Shared/Other/CustomAssertionsTests.swift
@@ -0,0 +1,42 @@
+//
+//  CustomAssertionsTests.swift
+//  SwiftyMocky
+//
+//  Created by Andrzej Michnia on 24/06/2019.
+//  Copyright © 2019 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import SwiftyMocky
+#if os(iOS)
+@testable import Mocky_Example_iOS
+#elseif os(tvOS)
+@testable import Mocky_Example_tvOS
+#elseif os(macOS)
+@testable import Mocky_Example_macOS
+#endif
+
+class CustomAssertionsTests: XCTestCase {
+
+    func test_throws_error_of_type_success() {
+        XCTAssertThrowsError(try throwErrorA(), of: TestError.self)
+    }
+
+    func test_throws_error_equatable_success() {
+        XCTAssertThrowsError(try throwErrorA(), error: TestError.a)
+        XCTAssertThrowsError(try throwErrorB(), error: TestError.b)
+    }
+
+    private enum TestError: Error, Equatable {
+        case a
+        case b
+    }
+
+    private func throwErrorA() throws -> Bool {
+        throw TestError.a
+    }
+
+    private func throwErrorB() throws -> Bool {
+        throw TestError.b
+    }
+}
