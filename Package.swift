0bbc4eb827d448b33c74f581fac4b448f07fc7d4
Andrzej Michnia <amichnia@gmail.com>
2019-11-08 12:42:58 +0100

    Updated installation and linux package description

--- Package.swift
+++ Package.swift
@@ -56,11 +56,11 @@ let package = Package(
         .library(name: "SwiftyMocky", targets: ["SwiftyMocky"]),
     ],
     dependencies: [
-        .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
-        .package(url: "https://github.com/jianstm/Crayon", from: "0.0.1"),
-        .package(url: "https://github.com/kylef/Commander", from: "0.8.0"),
-        .package(url: "https://github.com/kylef/PathKit", from: "0.0.1"),
-        .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
+       .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
+       .package(url: "https://github.com/jianstm/Crayon", from: "0.0.3"),
+       .package(url: "https://github.com/kylef/Commander", from: "0.8.0"),
+       .package(url: "https://github.com/kylef/PathKit", from: "1.0.0"),
+       .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
     ],
     targets: [
         .target(
