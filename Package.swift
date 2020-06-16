80baa5fa5a41301f2189f1d7835257bf5e262b1d
Andrzej Michnia <amichnia@google.com>
2020-06-16 15:25:18 +0200

    Removed CLI from main repo

--- Package.swift
+++ Package.swift
@@ -1,95 +1,19 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.1
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
-#if os(macOS)
 /// MacOS swiftymocky mackage
 let package = Package(
    name: "swiftymocky",
    products: [
-       .executable(name: "swiftymocky", targets: ["SwiftyMockyCLI"]),
        .library(name: "SwiftyMocky", targets: ["SwiftyMocky"]),
    ],
-   dependencies: [
-       .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
-       .package(url: "https://github.com/tuist/xcodeproj.git", from: "7.1.0"),
-       .package(url: "https://github.com/luoxiu/Chalk", .exact("0.0.7")),
-       .package(url: "https://github.com/kylef/Commander", .exact("0.9.1")),
-       .package(url: "https://github.com/kylef/PathKit", from: "1.0.0"),
-       .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
-   ],
+   dependencies: [],
    targets: [
        .target(
            name: "SwiftyMocky",
            path: "./Sources/Classes"
-           ),
-       .target(
-           name: "SwiftyMockyCLI",
-           dependencies: [
-               "Commander",
-               "SwiftyMockyCLICore"
-           ]),
-       .target(
-           name: "SwiftyMockyCLICore",
-           dependencies: [
-               "ShellOut",
-               "Chalk",
-               "XcodeProj",
-               "PathKit",
-               "Yams",
-           ]),
-       .testTarget(
-           name: "SwiftyMockyCLICoreTests",
-           dependencies: [
-               "SwiftyMockyCLICore",
-               "SwiftyMocky"
-           ]),
+        ),
    ]
 )
-#else
-/// Linux package. For now not really usable, until sourcery would be available too
-let package = Package(
-    name: "swiftymocky",
-    products: [
-        .executable(name: "swiftymocky", targets: ["SwiftyMockyCLI"]),
-        .library(name: "SwiftyMocky", targets: ["SwiftyMocky"]),
-    ],
-    dependencies: [
-       .package(url: "https://github.com/JohnSundell/ShellOut.git", from: "2.0.0"),
-       .package(url: "https://github.com/luoxiu/Chalk", .exact("0.0.7")),
-       .package(url: "https://github.com/kylef/Commander", .exact("0.9.1")),
-       .package(url: "https://github.com/kylef/PathKit", from: "1.0.0"),
-       .package(url: "https://github.com/jpsim/Yams.git", from: "2.0.0"),
-    ],
-    targets: [
-        .target(
-            name: "SwiftyMocky",
-            path: "./Sources/Classes"),
-        .target(
-            name: "SwiftyMockyCLI",
-            dependencies: [
-                "Commander",
-                "SwiftyMockyCLICore"
-            ]),
-        .target(
-            name: "SwiftyMockyCLICore",
-            dependencies: [
-                "ShellOut",
-                "Chalk",
-                "PathKit",
-                "Yams",
-            ],
-            exclude: [
-                "Commands/MacOS",
-                "Utils/MacOS",
-            ]),
-        .testTarget(
-            name: "SwiftyMockyCLICoreTests",
-            dependencies: [
-                "SwiftyMockyCLICore",
-                "SwiftyMocky"
-            ]),
-    ]
-)
-#endif
