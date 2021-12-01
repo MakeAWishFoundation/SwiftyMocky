47c5e5b9d1b9d2b25ca30e4664c53719b624ce7b
DMITRY KULAKOV <navartis@gmail.com>
2021-12-01 16:50:06 +0300

    Upgrade Yams version

--- Package.swift
+++ Package.swift
@@ -18,7 +18,7 @@ let package = Package(
         .package(url: "https://github.com/luoxiu/Chalk", .exact("0.2.1")),
         .package(url: "https://github.com/kylef/Commander", .upToNextMajor(from: "0.9.1")),
         .package(url: "https://github.com/kylef/PathKit", .upToNextMajor(from: "1.0.1")),
-        .package(url: "https://github.com/jpsim/Yams", .exact("3.0.1")),
+        .package(url: "https://github.com/jpsim/Yams", .upToNextMajor(from: "4.0.6")),
     ],
     targets: [
         // XCTest Runtime libraries
